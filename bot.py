bc3289b3b9e4b8ae03039464d9ec5c0b24f73575
WilliamCadenas21 <williamcadenas21@gmail.com>
2020-03-20 14:28:05 -0500

    follow and unfollow actions added

--- bot.py
+++ bot.py
@@ -2,10 +2,17 @@ from selenium import webdriver
 import os
 import time
 
+import configparser
+
+
 class InstagramBot:
 
     def __init__(self, username, password):
         """
+        Initializes an instance of the InstagramBot class.
+
+        Call the login method to authenticate a user with IG.
+
         Args:
             username:str: The Instagram username for a user
             password:str: The Instagram password for a user
@@ -22,7 +29,10 @@ class InstagramBot:
         self.driver = webdriver.Chrome('./chromedriver')
         self.login()
 
-    def login(self):    
+    def login(self):
+        """
+        Logs a user into Instagram via the web portal 
+        """    
         self.driver.get('{}/accounts/login/'.format(self.base_url))
         self.driver.maximize_window() #For maximizing window
         self.driver.implicitly_wait(20) #gives an implicit wait for 20 seconds
@@ -32,16 +42,39 @@ class InstagramBot:
 
         self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]/button').click()
         time.sleep(5)
-        self.driver.implicitly_wait(60)
 
     def nav_user(self, user):
-        self.driver.get('{}/{}'.format(self.base_url, user))
+        self.driver.get('{}/{}/'.format(self.base_url, user))
+
+    def follow_user(self, user):
+        self.nav_user(user)
+        time.sleep(2) # wait for load the page
+        follow_button = self.driver.find_element_by_xpath("//button[contains(text(), 'Follow')]")
+        follow_button.click()
+
+    def unfollow_user(self, user):
+        self.nav_user(user)
+        time.sleep(2) # wait for load the page 
+        unfollow_button_action = self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[2]/span/span[1]/button')
+        unfollow_button_action.click()                             #//*[@id="react-root"]/section/main/div/header/section/div[1]/div[2]/span/span[1]/button/div/span
+        unfollow_button_confirm = self.driver.find_element_by_xpath('/html/body/div[4]/div/div/div[3]/button[1]')
+        unfollow_button_confirm.click()
 
 
 if __name__ == '__main__':
-    temp1 = 'wcadenas@uninorte.edu.co'
-    temp2 = '/Tempinstagram123'
-    ig_bot = InstagramBot(temp1, temp2)
-    
-    ig_bot.nav_user('williamcad21')
+    config_path = './config.ini'
+    cparser = configparser.ConfigParser()
+    cparser.read(config_path)
 
+    username = cparser['AUTH']['USERNAME']
+    password = cparser['AUTH']['PASSWORD']
+    
+    ig_bot = InstagramBot(username, password)
+    #ig_bot.follow_user('mafelopez.03') #user that you want to navigate
+
+    
+    user = 'tonyrobbins'
+    #user = 'garyvee'
+    #ig_bot.follow_user(user) #user that you want to navigate
+    #time.sleep(5)
+    ig_bot.unfollow_user(user)
